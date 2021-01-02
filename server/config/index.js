428602fe5f62dcb9461d19434b3200fba68bc6d5
Pooja mishra <mishrapooja8128@gmail.com>
2021-01-02 18:27:54 +0530

    delete mongo url

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGO_URL: 'mongodb+srv://invoiceSystem:9s1asbk9weCocMyg@pm.1tahh.mongodb.net/invoiceSystem?retryWrites=true&w=majority',
+  MONGO_URL: 'mongo url',
   jwtSecret: 'abc123'
 };
